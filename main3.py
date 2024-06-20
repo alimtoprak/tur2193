d8d5a231fedc260f4d604ec92fbce3170bf5bcf8
alimtoprak <apolat80@gmail.com>
2024-06-20 21:19:13 +0300

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def parola(ctx,parola_uzunlugu)
+    await ctx.send(parola_yarat[parola_uzunlugu])
+
+bot.run("MTI1MjU4MzM0NDUyNTI3OTMzMw.GflGvl.NBnPbu8JszmyN1yjqcW0vIp5F9jidesVOgFvKc")
